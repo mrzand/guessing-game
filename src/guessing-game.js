a8ed5cd8a1469a3430e1c413e978f085596a0075
mrzand <zas15082015@gmail.com>
2017-02-04 23:22:56 +0300

    final result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,17 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.min= min + 1;
+        this.max = max - 1;
     }
-
     guess() {
-
+        this.result= Math.round((this.min + this.max) / 2);
+        return this.result;
     }
-
     lower() {
-
+        this.max= this.result - 1;
     }
-
     greater() {
-
+        this.min= this.result + 1;
     }
 }
-
 module.exports = GuessingGame;
